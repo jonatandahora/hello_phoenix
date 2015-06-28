0c1cbd867808ff15acf88236f086b69efb576133
Jonatan da Hora <jonatandahora@gmail.com>
2015-06-28 20:57:03 -0300

    added channels, and a test chat via socket

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -11,3 +11,24 @@ let App = {
 }
 
 export default App
+let chatInput         = $("#chat-input")
+let messagesContainer = $("#messages")
+
+let socket = new Socket("/ws")
+socket.connect()
+let chan = socket.chan("rooms:lobby", {})
+
+chatInput.on("keypress", event => {
+  if(event.keyCode === 13){
+    chan.push("new_msg", {body: chatInput.val()})
+    chatInput.val("")
+  }
+})
+
+chan.on("new_msg", payload => {
+  messagesContainer.append(`<br/>[${Date()}] ${payload.body}`)
+})
+
+chan.join().receive("ok", chan => {
+  console.log("Welcome to Phoenix Chat!")
+})
